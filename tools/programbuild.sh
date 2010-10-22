a04641d034b7212f9bfc8ddff91d049afad8950e
matias <matias>
2010-10-22 19:45:05 +0000

    Adding the -fno-zero-initialized-in-bss gcc option

--- tools/programbuild.sh
+++ tools/programbuild.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-#export PATH=$PATH:/opt/mipstools/bin
-#mipseb-netbsdelf-pcc -O2 -c program.c
-#mipseb-netbsdelf-ld -T /opt/mipstools/mipseb-netbsdelf/lib/ldscripts/emulator.x program.o -o program.bin
-mips-linux-gnu-gcc -mips1 -mno-abicalls -mno-xgot -O2 -Wall -c program.c
+mips-linux-gnu-gcc -mips1 -mno-abicalls -mno-xgot -O2 -fno-zero-initialized-in-bss -Wall -c program.c
 mips-linux-gnu-ld -T tools/emulator.x program.o -o program.bin
 tools/bin2vmem.py < program.bin > program.mem
