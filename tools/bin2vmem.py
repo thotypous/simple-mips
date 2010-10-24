988f451ad4389422b918f279327e5e0eccdd5009
matias <matias>
2010-10-24 01:23:57 +0000

    Option for bin2vmem to load the bin file at an address != 0x400

--- tools/bin2vmem.py
+++ tools/bin2vmem.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import sys, struct
-print '@100'
+load_at = 0x400
+if len(sys.argv) == 2:
+    load_at = int(sys.argv[1],16)
+print '@%x'%(load_at>>2)
 while True:
     data = sys.stdin.read(4)
     if len(data) != 4:
